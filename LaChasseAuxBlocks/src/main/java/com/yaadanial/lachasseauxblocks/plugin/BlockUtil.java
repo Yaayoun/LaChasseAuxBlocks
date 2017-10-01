86f9b9f4545ffa0a354b2a882d5d8696a90973b0
Valdanial <andy_martel@hotmail.fr>
2017-10-01 19:05:31 +0200

    Readding new BlockUtil.java file

--- LaChasseAuxBlocks/src/main/java/com/yaadanial/lachasseauxblocks/plugin/BlockUtil.java
+++ LaChasseAuxBlocks/src/main/java/com/yaadanial/lachasseauxblocks/plugin/BlockUtil.java
@@ -1,51 +1,48 @@
 package com.yaadanial.lachasseauxblocks.plugin;
 
-import java.util.Random;
-
 import org.bukkit.Material;
-import org.bukkit.World;
-import org.bukkit.block.Block;
-import org.bukkit.entity.Player;
 
-public class BlockUtil {
+/**
+ * Classe de gestion des blocks
+ * 
+ * @author Yaadanial
+ *
+ */
+public class BlockTypeData {
 
-	/**
-	 * Créer un Block de Granite
-	 * 
-	 * @param pl le Joueur
-	 * @param w le Monde
-	 * @param type le type du block à créer
-	 * @param data l'extension du block à créer
-	 * @param x la position en X du Block
-	 * @param y la position en Y du Block
-	 * @param z la position en Z du Block
-	 * @return le block créé
-	 */
-	public static Block createABlock(Player pl, World w, Material type, int data, int x, int y, int z) {
-		Block block = w.getBlockAt(pl.getLocation().getBlockX() + x, pl.getLocation().getBlockY() + y, pl.getLocation().getBlockZ() + z);
-		block.setType(type);
-		block.setData((byte) data);
-		return block;
+	private Material material;
+	private Integer data;
+	private String name;
+
+	public BlockTypeData(Material material, Integer data, String name) {
+		super();
+		this.material = material;
+		this.data = data;
+		this.name = name;
 	}
 
-	/**
-	 * Créer un Block Aléatoire
-	 * 
-	 * @param pl le Joueur
-	 * @param w le Monde
-	 * @param x la position en X du Block
-	 * @param y la position en Y du Block
-	 * @param z la position en Z du Block
-	 * @return le block créé
-	 */
-	public static Block createRandomBlock(Player pl, World w, int x, int y, int z) {
-		Block block = w.getBlockAt(pl.getLocation().getBlockX() + x, pl.getLocation().getBlockY() + y, pl.getLocation().getBlockZ() + z);
-		Random random = new Random();
-		Material[] materials = Material.values();
-		int size = materials.length;
-		int index = random.nextInt(size);
-		Material randomMaterial = materials[index];
-		block.setType(randomMaterial);
-		return block;
+	public String getName() {
+		return name;
 	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public Material getMaterial() {
+		return material;
+	}
+
+	public void setMaterial(Material material) {
+		this.material = material;
+	}
+
+	public Integer getData() {
+		return data;
+	}
+
+	public void setData(Integer data) {
+		this.data = data;
+	}
+
 }
