f2ba7dd62e62affaad5584164c7074d2f724791b
Valdanial <andy_martel@hotmail.fr>
2017-10-01 19:03:43 +0200

    Readding new BlockTypeData.java file

--- LaChasseAuxBlocks/src/main/java/com/yaadanial/lachasseauxblocks/plugin/BlockTypeData.java
+++ LaChasseAuxBlocks/src/main/java/com/yaadanial/lachasseauxblocks/plugin/BlockTypeData.java
@@ -2,15 +2,31 @@ package com.yaadanial.lachasseauxblocks.plugin;
 
 import org.bukkit.Material;
 
+/**
+ * Classe de gestion des blocks
+ * 
+ * @author Yaadanial
+ *
+ */
 public class BlockTypeData {
 
 	private Material material;
 	private Integer data;
+	private String name;
 
-	public BlockTypeData(Material material, Integer data) {
+	public BlockTypeData(Material material, Integer data, String name) {
 		super();
 		this.material = material;
 		this.data = data;
+		this.name = name;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
 	}
 
 	public Material getMaterial() {
